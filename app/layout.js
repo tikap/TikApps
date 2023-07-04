bc1f6433c0c1be397c95cb02e52101ced91a5af4
tikap <tikapradhan095@gmail.com>
2023-07-04 01:06:33 -0400

    Setup header and footer skeleton, added BMI page and route

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,25 @@
 // These styles apply to every route in the application
 import './globals.css'
 
+// Link import
+import Link from 'next/link'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <h1>Header</h1>
+          <Link href="/">Home</Link>
+          <Link href="/bmi"> BMI</Link>
+        </header>
+        
+        {children}
+        
+        <footer>
+          Footer
+        </footer>
+      </body>
     </html>
   );
 }
