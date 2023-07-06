63b986c1d0d1b1096d0c620938e2f3b83e126d9b
tikap <tikapradhan095@gmail.com>
2023-07-05 22:31:13 -0400

    Added header content and bmi link

--- app/header/header.js
+++ app/header/header.js
@@ -1,9 +1,99 @@
-export default function Header(){
-    return (
-        <header>
-            <div class="bg-purple-600">
-                Header.
-            </div>
-        </header>
-    )   
-}   
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav
+        class="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+        data-te-navbar-ref
+      >
+        <div class="flex w-full flex-wrap items-center justify-between px-3">
+          <div class="flex items-center">
+            <button
+              class="border-0 bg-transparent px-2 text-xl leading-none transition-shadow duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white lg:hidden"
+              type="button"
+              data-te-collapse-init
+              data-te-target="#navbarSupportedContentY"
+              aria-controls="navbarSupportedContentY"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="[&>svg]:w-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-7 w-7"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
+
+          <div
+            class="!visible hidden grow basis-[100%] items-center lg:!flex lg:basis-auto"
+            id="navbarSupportedContentY"
+            data-te-collapse-item
+          >
+            <ul
+              class="mr-auto flex flex-col lg:flex-row"
+              data-te-navbar-nav-ref
+            >
+              <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link
+                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  href="./"
+                  data-te-nav-link-ref
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Home
+                </Link>
+              </li>
+              <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link
+                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  href="./bmi"
+                  data-te-nav-link-ref
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  BMI Calculator
+                </Link>
+              </li>
+              <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link
+                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  href="./about"
+                  data-te-nav-link-ref
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  About
+                </Link>
+              </li>
+              <li class="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                <Link
+                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  href="./contact"
+                  data-te-nav-link-ref
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
