e5bec01bc56a1b430c6fae6d66d7b5f88f9a680b
tikap <tikapradhan095@gmail.com>
2023-07-05 22:41:51 -0400

    Fix disappearing header items in mobile

--- app/header/header.js
+++ app/header/header.js
@@ -8,37 +8,8 @@ export default function Header() {
         data-te-navbar-ref
       >
         <div class="flex w-full flex-wrap items-center justify-between px-3">
-          <div class="flex items-center">
-            <button
-              class="border-0 bg-transparent px-2 text-xl leading-none transition-shadow duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white lg:hidden"
-              type="button"
-              data-te-collapse-init
-              data-te-target="#navbarSupportedContentY"
-              aria-controls="navbarSupportedContentY"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="[&>svg]:w-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="h-7 w-7"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              </span>
-            </button>
-          </div>
-
           <div
-            class="!visible hidden grow basis-[100%] items-center lg:!flex lg:basis-auto"
+            class="!visible grow basis-[100%] items-center lg:!flex lg:basis-auto"
             id="navbarSupportedContentY"
             data-te-collapse-item
           >
