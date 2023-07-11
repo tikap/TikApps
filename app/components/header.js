ef2a24ed3fb06f56a42c4df5bbadfde796c16fd7
tikap <tikapradhan095@gmail.com>
2023-07-11 00:01:15 -0400

    Apply header colors to theme and remove dark light theme interaction

--- app/components/header.js
+++ app/components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <nav
-        class="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+        class="relative flex w-full items-center justify-between bg-black py-2 text-neutral-200 shadow-lg hover:text-neutral-400 focus:text-neutral-700 md:flex-wrap md:justify-start "
         data-te-navbar-ref
       >
         <div class="flex w-full flex-wrap items-center justify-between px-3">
@@ -19,7 +19,7 @@ export default function Header() {
             >
               <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
                 <Link
-                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  class="block transition duration-150 ease-in-out hover:text-neutral-400 focus:text-white disabled:text-black/30 lg:p-2 [&.active]:text-black/90"
                   href="./"
                   data-te-nav-link-ref
                   data-te-ripple-init
@@ -30,7 +30,7 @@ export default function Header() {
               </li>
               <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
                 <Link
-                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  class="block transition duration-150 ease-in-out hover:text-neutral-400 focus:text-white disabled:text-black/30 lg:p-2 [&.active]:text-black/90"
                   href="./bmi"
                   data-te-nav-link-ref
                   data-te-ripple-init
@@ -41,7 +41,7 @@ export default function Header() {
               </li>
               <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
                 <Link
-                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  class="block transition duration-150 ease-in-out hover:text-neutral-400 focus:text-white disabled:text-black/30 lg:p-2 [&.active]:text-black/90"
                   href="./about"
                   data-te-nav-link-ref
                   data-te-ripple-init
@@ -52,7 +52,7 @@ export default function Header() {
               </li>
               <li class="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
                 <Link
-                  class="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                  class="block transition duration-150 ease-in-out hover:text-neutral-400 focus:text-white disabled:text-black/30 lg:p-2 [&.active]:text-black/90"
                   href="./contact"
                   data-te-nav-link-ref
                   data-te-ripple-init
