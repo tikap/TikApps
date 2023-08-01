764e5741723e95add331b9eef9d70dd677b1f857
tikap <tikapradhan095@gmail.com>
2023-07-31 23:53:24 -0400

    Add BMI result div

--- app/bmi/bmiResult.js
+++ app/bmi/bmiResult.js
@@ -1,3 +1,10 @@
 export default function BmiResult({ resultValue }) {
-  return <div className="block m-10 font-bold">{resultValue}</div>;
+  return (
+    <div class="flex justify-center">
+      <div className="w-64 p-2 my-8 bg-gray-800">
+        <div className="block m-8">Your BMI:</div>
+        <div className="block m-8 font-bold text-lg">{resultValue}</div>
+      </div>
+    </div>
+  );
 }
