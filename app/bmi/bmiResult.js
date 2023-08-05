1421e3fb40c01ebeca0600bc4135d497cbd22f1f
tikap <tikapradhan095@gmail.com>
2023-08-04 23:36:40 -0400

    Added weight categories to bmi results

--- app/bmi/bmiResult.js
+++ app/bmi/bmiResult.js
@@ -1,9 +1,29 @@
 export default function BmiResult({ resultValue }) {
+  var weightCategory = "";
+
+  switch (true) {
+    case resultValue >= 30:
+      weightCategory = "Obese";
+      break;
+    case resultValue >= 25:
+      weightCategory = "Overweight";
+      break;
+    case resultValue >= 18.5:
+      weightCategory = "Normal";
+      break;
+    case resultValue >= 0:
+      weightCategory = "Underweight";
+      break;
+    default:
+      weightCategory = "";
+  }
+
   return (
-    <div class="flex justify-center">
+    <div className="flex justify-center">
       <div className="w-64 p-2 my-8 bg-gray-800">
         <div className="block m-8">Your BMI:</div>
         <div className="block m-8 font-bold text-lg">{resultValue}</div>
+        <div className="block m-8">{weightCategory}</div>
       </div>
     </div>
   );
