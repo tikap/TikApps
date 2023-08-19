00c4bcc5dd94e87adb5f3fa8af32fa14446a1ac3
tikap <tikapradhan095@gmail.com>
2023-08-19 17:34:44 -0400

    More UI adjustments for numberInput and RadioTwoOptions

--- app/components/forms/NumberInput.js
+++ app/components/forms/NumberInput.js
@@ -11,7 +11,7 @@ export default function NumberInput({
   return (
     <div className="relative my-8">
       <label className="flex justify-center items-center">
-        <span className="mx-2 text-sm">{label}:</span>
+        <span className="mx-2">{label}:</span>
         <input
           type="number"
           onChange={onValueChange}
@@ -25,7 +25,7 @@ export default function NumberInput({
             " h-6 rounded-md text-black text-center text-sm bg-gray-200 focus:bg-gray-100"
           }
         />
-        <span className="mx-2 text-sm">{unit}</span>
+        <span className="mx-2">{unit}</span>
       </label>
     </div>
   );
--- app/components/forms/RadioTwoOptions.js
+++ app/components/forms/RadioTwoOptions.js
@@ -10,13 +10,13 @@ export default function RadioTwoOptions({
           className="relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
           type="radio"
           name={radioGroupName}
-          id="radio1"
+          id={radioGroupName + "radio1"}
           value="option1"
           defaultChecked
         />
         <label
           className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer"
-          htmlFor="radio1"
+          htmlFor={radioGroupName + "radio1"}
         >
           {optionOneName}
         </label>
@@ -27,12 +27,12 @@ export default function RadioTwoOptions({
           className="relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
           type="radio"
           name={radioGroupName}
-          id="radio2"
+          id={radioGroupName + "radio2"}
           value="option2"
         />
         <label
           className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer"
-          htmlFor="radio2"
+          htmlFor={radioGroupName + "radio2"}
         >
           {optionTwoName}
         </label>
