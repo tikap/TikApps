9ee53a16f6a2cdef87ae71f0658dc6c2cb5243cf
tikap <tikapradhan095@gmail.com>
2023-09-09 23:55:33 -0400

    Add required validations for contact form

--- app/contact/ContactForm.js
+++ app/contact/ContactForm.js
@@ -30,6 +30,7 @@ export default function ContactForm({
                 id="exampleInput90"
                 placeholder="Name"
                 onChange={nameChangeHandler}
+                required
               />
               <label
                 className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -45,6 +46,7 @@ export default function ContactForm({
                 id="exampleInput91"
                 placeholder="Email address"
                 onChange={emailChangeHandler}
+                required
               />
               <label
                 className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -60,6 +62,7 @@ export default function ContactForm({
                 rows="3"
                 placeholder="Your message"
                 onChange={messageChangeHandler}
+                required
               ></textarea>
               <label
                 htmlFor="exampleFormControlTextarea1"
