469a761bf1c4ff4653f76fb0a6eec17ca06eb016
tikap <tikapradhan095@gmail.com>
2024-05-12 02:05:50 -0400

    UI layout polish

--- app/components/content/UnderConstruction.js
+++ app/components/content/UnderConstruction.js
@@ -5,10 +5,14 @@ import TrafficCone from "../../resources/images/traffic_cone_32px.png";
 
 export default function UnderConstruction() {
   return (
-    <div className="flex justify-center space-x-8">
-      <Image src={TrafficCone} alt="Traffic Cone Icon" />
+    <div className="py-20 flex justify-center space-x-8">
+      <div className="min-w-max">
+        <Image src={TrafficCone} alt="Traffic Cone Icon" />
+      </div>
       <div className="text-lg"> Work in progress ...</div>
-      <Image src={TrafficCone} alt="Traffic Cone Icon" />
+      <div className="min-w-max">
+        <Image src={TrafficCone} alt="Traffic Cone Icon" />
+      </div>
     </div>
   );
 }
--- app/components/wrappers/mainwrapper.js
+++ app/components/wrappers/mainwrapper.js
@@ -1,7 +1,7 @@
 export default function MainWrapper({ wrappedComponent }) {
   return (
     <div
-      className="w-auto h-auto 
+      className="w-auto h-fit min-h-[65vh]
       bg-gradient-to-r from-gray-950 to-gray-900 
     text-neutral-200 p-12 text-center"
     >
